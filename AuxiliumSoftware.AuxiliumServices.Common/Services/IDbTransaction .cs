4fd245393914903cf1791316b517222144c734aa
Cerys <cerysamberlewis@gmail.com>
2026-01-20 11:32:21 +0000

    Delete IDbTransaction .cs

--- AuxiliumSoftware.AuxiliumServices.Common/Services/IDbTransaction .cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AuxiliumSoftware.AuxiliumServices.Common.Services.Interfaces
-{
-    public interface IDbTransaction : IAsyncDisposable
-    {
-        Task CommitAsync();
-        Task RollbackAsync();
-    }
-}
