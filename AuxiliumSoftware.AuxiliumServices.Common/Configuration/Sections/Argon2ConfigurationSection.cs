3c7ed3bedb1fc59b6693014e9ec78ff7db552b7b
Cerys <cerysamberlewis@gmail.com>
2026-01-27 14:47:58 +0000

    Argon2 stuff now in config

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Configuration/Sections/Argon2ConfigurationSection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Configuration.Sections
+{
+    public class Argon2ConfigurationSection
+    {
+        public required int MemoryCost { get; set; }
+        public required int TimeCost { get; set; }
+        public required int Parallelism { get; set; }
+        public required int HashLength { get; set; }
+        public required int SaltLength { get; set; }
+    }
+}
