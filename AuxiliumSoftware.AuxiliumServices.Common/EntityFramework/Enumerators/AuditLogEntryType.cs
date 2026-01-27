1f29fedc6b08a21c641e358047060a3d0b3c93f8
Cerys <cerysamberlewis@gmail.com>
2026-01-27 20:27:54 +0000

    audit log and system bulletin ef stuff

---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Enumerators/AuditLogEntryType.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum AuditLogEntryType
+{
+    [JsonPropertyName("/service/api/start")]
+    Service_API_Start
+}
---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Enumerators/SystemBulletinMessageSeverity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum SystemBulletinMessageSeverity
+    {
+        [JsonPropertyName("informational")]
+        Informational,
+
+        [JsonPropertyName("warning")]
+        Warning,
+
+        [JsonPropertyName("critical")]
+        Critical,
+    }
+}
