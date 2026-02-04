b58aca8cec734b3aa3c8d54a113f632f02447639
Cerys <cerysamberlewis@gmail.com>
2026-02-04 16:04:07 +0000

    more documentation fixes

--- AuxiliumSoftware.AuxiliumServices.Common/Utilities/HashingUtilities.cs
+++ AuxiliumSoftware.AuxiliumServices.Common/Utilities/HashingUtilities.cs
@@ -8,18 +8,22 @@ namespace AuxiliumSoftware.AuxiliumServices.Common.Utilities
     /// </summary>
     public class HashingUtilities
     {
-        /**
-         * Hashes a given string into SHA256 and returns the hex representation.
-         */
+        /// <summary>
+        /// Hashes a given string into SHA256 and returns the hex representation.
+        /// </summary>
+        /// <param name="input">The string to hash.</param>
+        /// <returns>The hashed string.</returns>
         public static string SHA256Hash(string input)
         {
             var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(input));
             return Convert.ToHexString(bytes).ToLowerInvariant();
         }
 
-        /**
-         * Hashes a given byte array into SHA256 and returns the hex representation.
-         */
+        /// <summary>
+        /// Hashes a given byte array into SHA256 and returns the hex representation.
+        /// </summary>
+        /// <param name="data">The byte array to hash.</param>
+        /// <returns>The hashed string.</returns>
         public static string SHA256Hash(byte[] data)
         {
             var hash = SHA256.HashData(data);
