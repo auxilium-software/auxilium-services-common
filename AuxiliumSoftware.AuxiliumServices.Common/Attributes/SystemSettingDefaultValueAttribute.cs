556d1dea9c0e3a00dbf242aff9d9ecbcabb233d6
Cerys <cerysamberlewis@gmail.com>
2026-02-07 23:57:53 +0000

    Create SystemSettingDefaultValueAttribute.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Attributes/SystemSettingDefaultValueAttribute.cs
@@ -0,0 +1,16 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators;
+using System;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    internal class SystemSettingDefaultValueAttribute : Attribute
+    {
+        public object DefaultValue { get; }
+
+        public SystemSettingDefaultValueAttribute(object defaultValue)
+        {
+            DefaultValue = defaultValue;
+        }
+    }
+}
