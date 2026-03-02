1fd58e202175fb6ee7807599f0ef0c9c42253e8e
Cerys <cerysamberlewis@gmail.com>
2026-03-02 15:18:42 +0000

    extension classes

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Extensions/CasbinPolicyActionTypeEnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Extensions
+{
+    public static class CasbinPolicyActionTypeEnumExtensions
+    {
+        public static string ToPolicyString(this CasbinPolicyActionTypeEnum type)
+        {
+            var field = typeof(CasbinPolicyActionTypeEnum).GetField(type.ToString());
+            var attr = field?.GetCustomAttribute<JsonPropertyNameAttribute>();
+            return attr?.Name ?? type.ToString();
+        }
+    }
+}
