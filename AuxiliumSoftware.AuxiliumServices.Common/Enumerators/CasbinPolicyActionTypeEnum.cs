df8fd11164913de7d5a0796c283b603a03414c73
Cerys <cerysamberlewis@gmail.com>
2026-03-03 14:23:44 +0000

    Create CasbinPolicyActionTypeEnum.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Enumerators/CasbinPolicyActionTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Enumerators
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum CasbinPolicyActionTypeEnum
+    {
+        [JsonPropertyName("PolicyAction.Read")]
+        Read,
+
+        [JsonPropertyName("PolicyAction.Write")]
+        Write,
+    }
+}
