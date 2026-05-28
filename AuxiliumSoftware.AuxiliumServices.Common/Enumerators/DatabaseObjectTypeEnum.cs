858f779c0e61d1f16ee9bf2a493321abe4aee4e4
Cerys <cerysamberlewis@gmail.com>
2026-05-28 16:29:39 +0100

    Update DatabaseObjectTypeEnum.cs

--- AuxiliumSoftware.AuxiliumServices.Common/Enumerators/DatabaseObjectTypeEnum.cs
+++ AuxiliumSoftware.AuxiliumServices.Common/Enumerators/DatabaseObjectTypeEnum.cs
@@ -5,52 +5,23 @@ namespace AuxiliumSoftware.AuxiliumServices.Common.Enumerators
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum DatabaseObjectTypeEnum
     {
-        [JsonPropertyName("System.SettingEntry")]
-        System_SettingEntry,
 
-        [JsonPropertyName("System.Waf.IpWhitelistEntry")]
-        System_Waf_IpWhitelistEntry,
-        [JsonPropertyName("System.Waf.IpBlacklistEntry")]
-        System_Waf_IpBlacklistEntry,
-        [JsonPropertyName("System.Waf.UserWhitelistEntry")]
-        System_Waf_UserWhitelistEntry,
-        [JsonPropertyName("System.Waf.UserBlacklistEntry")]
-        System_Waf_UserBlacklistEntry,
-
-        [JsonPropertyName("System.BulletinEntry")]
-        System_BulletinEntry,
-
-
-
-        [JsonPropertyName("User")]
-        User,
-        [JsonPropertyName("User.AdditionalProperty")]
-        User_AdditionalProperty,
-        [JsonPropertyName("User.File")]
-        User_File,
-        [JsonPropertyName("User.RefreshToken")]
-        User_RefreshToken,
-        [JsonPropertyName("User.TotpRecoveryCode")]
-        User_TotpRecoveryCode,
-        [JsonPropertyName("User.WemwbsAssessment")]
-        User_WemwbsAssessment,
-
-
-
-        [JsonPropertyName("Case")]
+        [JsonPropertyName("Case.Cases")]
         Case,
-        [JsonPropertyName("CaseAdditionalProperty")]
+        [JsonPropertyName("Case.AdditionalProperty")]
         Case_AdditionalProperty,
-        [JsonPropertyName("CaseWorker")]
-        Case_Worker,
-        [JsonPropertyName("CaseClient")]
+        [JsonPropertyName("Case.Client")]
         Case_Client,
-        [JsonPropertyName("CaseMessage")]
-        Case_Message,
-        [JsonPropertyName("CaseFile")]
+        [JsonPropertyName("Case.File")]
         Case_File,
-        [JsonPropertyName("CaseTodo")]
+        [JsonPropertyName("Case.Message")]
+        Case_Message,
+        [JsonPropertyName("Case.Todo")]
         Case_Todo,
+        [JsonPropertyName("Case.Worker")]
+        Case_Worker,
+
+
 
 
 
@@ -66,5 +37,40 @@ namespace AuxiliumSoftware.AuxiliumServices.Common.Enumerators
         Log_SystemBulletinEntryViewEventEntry,
         [JsonPropertyName("Log.UserModificationEventEntry")]
         Log_UserModificationEventEntry,
+
+
+
+
+
+        [JsonPropertyName("System.BulletinEntry")]
+        System_BulletinEntry,
+        [JsonPropertyName("System.SettingEntry")]
+        System_SettingEntry,
+        [JsonPropertyName("System.Waf.IpBlacklistEntry")]
+        System_Waf_IpBlacklistEntry,
+        [JsonPropertyName("System.Waf.IpWhitelistEntry")]
+        System_Waf_IpWhitelistEntry,
+        [JsonPropertyName("System.Waf.UserBlacklistEntry")]
+        System_Waf_UserBlacklistEntry,
+        [JsonPropertyName("System.Waf.UserWhitelistEntry")]
+        System_Waf_UserWhitelistEntry,
+
+
+
+
+        [JsonPropertyName("User.Users")]
+        User,
+        [JsonPropertyName("User.AdditionalProperty")]
+        User_AdditionalProperty,
+        [JsonPropertyName("User.File")]
+        User_File,
+        [JsonPropertyName("User.PasswordSetToken")]
+        User_PasswordSetToken,
+        [JsonPropertyName("User.RefreshToken")]
+        User_RefreshToken,
+        [JsonPropertyName("User.TotpRecoveryCode")]
+        User_TotpRecoveryCode,
+        [JsonPropertyName("User.WemwbsAssessment")]
+        User_WemwbsAssessment,
     }
 }
