18e522b2d520f83772b0349dfe965da69504988f
Cerys <cerysamberlewis@gmail.com>
2026-05-28 16:16:37 +0100

    Update AuxiliumDbContext.cs

--- AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/AuxiliumDbContext.cs
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/AuxiliumDbContext.cs
@@ -113,7 +113,7 @@ public class AuxiliumDbContext : DbContext
             entity.HasOne(e => e.LastUpdatedByUser)                 .WithMany()                                                 .HasForeignKey(e => e.LastUpdatedBy)    .OnDelete(DeleteBehavior.SetNull);
             entity.HasOne(e => e.Case)                              .WithMany(c => c.AdditionalProperties)                      .HasForeignKey(e => e.CaseId);
 
-            entity.HasIndex(e => new { e.CaseId, e.UrlSlug })       .IsUnique();
+            entity.HasIndex(e => new { e.CaseId })       .IsUnique();
         });
 
         // case_clients
@@ -611,7 +611,7 @@ public class AuxiliumDbContext : DbContext
             entity.HasOne(e => e.LastUpdatedByUser)                 .WithMany()                                                 .HasForeignKey(e => e.LastUpdatedBy)    .OnDelete(DeleteBehavior.SetNull);
             entity.HasOne(e => e.User)                              .WithMany(u => u.AdditionalProperties)                      .HasForeignKey(e => e.UserId);
 
-            entity.HasIndex(e => new { e.UserId, e.UrlSlug })          .IsUnique();
+            entity.HasIndex(e => new { e.UserId })          .IsUnique();
         });
 
         // user_files
